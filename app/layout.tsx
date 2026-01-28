27b2a62258967d269a5595940791b42e808140bd
SWATVSS107 <vedant.tendulkar13@gmail.com>
2026-01-28 17:09:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,3 +50,12 @@ export default function RootLayout({
     </html>
   )
 }
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JDXMK0X6YS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JDXMK0X6YS');
+</script>
